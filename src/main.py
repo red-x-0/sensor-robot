f1d7ed2cb5c3a3a28809a8d78677f14427654b38
Sief Ali <sief4136@gmail.com>
2025-04-26 20:02:56 +0300

    Initialize Buzzer component on Pin 15.

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from machine import Pin
+from buzzer import Buzzer
 
 led = Pin(12, Pin.OUT)
 
 button = Pin(27, Pin.IN, Pin.PULL_UP)
 
 buzzerPin = 15
+buzzer = Buzzer(buzzerPin)
 
 def main():
   led.value(0)
